d7108505658b45ee005a61616472ed85e50b3d06
Joey <joey@evolution-x.org>
2026-03-14 16:42:58 +0900

    Revert "sm8350-common: Move powerhint copy operation to device-specific tree"
    This reverts commit cf01e1d300228e33a5d5633167ca34da77dc9f42.
    
    Revert "sm8350-common: Mirror shima powerhint from lahaina"
    
    This reverts commit c6eb2d4226f825d35b5acccc9cc8e2cacc6fcd7c.
    
    Signed-off-by: THE-EGO-999-GT <theego999.me@gmail.com>

--- common.mk
+++ common.mk
@@ -385,6 +385,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 $(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libpowermode-ext-xiaomi)
 
 # PowerShare

9c6c042a20bf59c00626361762d997e853a389d7
Nolen Johnson <johnsonnolen@gmail.com>
2026-06-25 19:07:21 -0400

    sm8350-common: Use Clang r563880c for kernel build
    * Doesn't boot with default Clang version.
    
    Change-Id: I35765e296044eef428d01bbf5a21e91f1897704e
    Signed-off-by: THE-EGO-999-GT <theego999.me@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -86,6 +86,8 @@ BOARD_BOOT_HEADER_VERSION := 3
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 TARGET_KERNEL_ADDITIONAL_FLAGS := TARGET_PRODUCT=$(PRODUCT_DEVICE)
+TARGET_KERNEL_CLANG_VERSION := r563880c
+TARGET_KERNEL_CLANG_PATH := $(abspath .)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
 TARGET_KERNEL_NO_GCC := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8350
 TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig vendor/debugfs.config vendor/xiaomi_QGKI.config

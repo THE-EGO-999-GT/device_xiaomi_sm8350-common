61504e015d200e91dd3e98281a336b16a2951c54
Never-Alive <livinglucifer0@gmail.com>
2026-06-28 10:30:00 +0530

    sm8350-common: Switch back to GL for default graphic api - Yeah it is color distortion.
    Signed-off-by: Never-Alive <livinglucifer0@gmail.com>

--- common.mk
+++ common.mk
@@ -224,8 +224,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 
 # Graphics
-TARGET_USES_VULKAN := true
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml

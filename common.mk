LineageOS/android_device_xiaomi_sm8350-common

sm8350-common: Build missing libraries for 14 QPR3
Change-Id: I6a84526ca1e9f4e5140c8068646433cab3fe85b2

common.mk
@@ -32,6 +32,9 @@ PRODUCT_PACKAGES += \
     libvolumelistener
 
 PRODUCT_PACKAGES += \
+    libaudioroute.vendor
+
+PRODUCT_PACKAGES += \
     audioadsprpcd \
     audio.r_submix.default \
     audio.usb.default
@@ -127,6 +130,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libMegviiFacepp-0.5.2 \
     libmegface
+
+PRODUCT_PACKAGES += \
+    liblz4.vendor \
+    libpng.vendor \
+    libutilscallstack.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera_cnf.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera_cnf.txt
@@ -430,6 +438,12 @@ PRODUCT_PACKAGES += \
     libjson \
     libprotobuf-cpp-lite-3.9.1-vendorcompat \
     librmnetctl
+
+PRODUCT_PACKAGES += \
+    libcurl.vendor \
+    libjsoncpp.vendor \
+    libsqlite.vendor \
+    libssl.vendor
 
 # Sensors
 PRODUCT_PACKAGES += \

THE-EGO-999-GT/device_xiaomi_sm8350-common

common.mk
@@ -32,6 +32,9 @@ PRODUCT_PACKAGES += \
     libvolumelistener
 
 PRODUCT_PACKAGES += \
+    libaudioroute.vendor
+
+PRODUCT_PACKAGES += \
     audioadsprpcd \
     audio.r_submix.default \
     audio.usb.default
@@ -127,6 +130,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libMegviiFacepp-0.5.2 \
     libmegface
+
+PRODUCT_PACKAGES += \
+    liblz4.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera_cnf.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera_cnf.txt
@@ -395,6 +401,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     vendor.qti.hardware.perf@2.2.vendor
 
+PRODUCT_PACKAGES += \
+    libssl.vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
@@ -430,6 +439,11 @@ PRODUCT_PACKAGES += \
     libjson \
     libprotobuf-cpp-lite-3.9.1-vendorcompat \
     librmnetctl
+
+PRODUCT_PACKAGES += \
+    libcurl.vendor \
+    libjsoncpp.vendor \
+    libsqlite.vendor
 
 # Sensors
 PRODUCT_PACKAGES += \
@@ -553,6 +567,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libnl \
     libwfdaac_vendor
+
+PRODUCT_PACKAGES += \
+    libpng.vendor
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
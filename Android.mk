renoir-development/android_device_xiaomi_sm8350-common

sm8350-common: Add support for odin
Change-Id: I7a657b9a8c5732860bb120472f14fe4c83fb6e22

Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter haydn renoir venus,$(TARGET_DEVICE)),)
+ifneq ($(filter haydn odin renoir venus,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)

setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "haydn renoir venus"
+write_headers "haydn odin renoir venus"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true

THE-EGO-999-GT/device_xiaomi_sm8350-common

Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter haydn lisa venus,$(TARGET_DEVICE)),)
+ifneq ($(filter haydn lisa odin venus,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)

setup-makefiles.sh
@@ -26,7 +26,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true
 
 # Copyright headers and guards
-write_headers "haydn lisa venus"
+write_headers "haydn lisa odin venus"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
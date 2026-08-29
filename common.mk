626cb41ca36e3ad0d977eb2ab8e880cfed2f5d82
Michael Bestas <mkbestas@lineageos.org>
2026-08-29 10:19:42 +0300

    sm8350-common: Drop UFFD flags
    See: Ibb2c5e9191744f6e15f6bced337ba7d0ed9681ca
    Change-Id: I40424ec785b2f1674a5eefe2150bda5c0a170e61
    Signed-off-by: THE-EGO-999-GT <theego999.me@gmail.com>

--- common.mk
+++ common.mk
@@ -284,9 +284,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
-# Kernel
-PRODUCT_ENABLE_UFFD_GC := true
-
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default

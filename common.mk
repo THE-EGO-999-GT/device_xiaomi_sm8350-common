08d61ca58a2d8d6529ff03f6595031f287f88e73
THE-EGO-999-GT <theego999.me@gmail.com>
2026-09-13 23:52:03 +0600

    sm8350-common: Build and configure xiaomi touch hal
    Signed-off-by: THE-EGO-999-GT <theego999.me@gmail.com>

--- common.mk
+++ common.mk
@@ -465,6 +465,11 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
 
 # Touchscreen
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.xiaomi
+
+$(call soong_config_set, XIAOMI_TOUCH, HIGH_TOUCH_POLLING_PATH, /sys/devices/virtual/touch/touch_dev/bump_sample_rate)
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 

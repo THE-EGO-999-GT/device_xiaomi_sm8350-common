LineageOS/android_device_sony_sm8350-common

sm8350-common: Disable `thumbnail_block_model` of libstagefright
Change-Id: I86f92335af282d3de508150bbbcb3026993ec48c

common.mk
@@ -290,6 +290,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_lahaina_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_lahaina.xml \
     $(LOCAL_PATH)/media/media_codecs_lahaina_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_lahaina_vendor.xml
 
+$(call soong_config_set,stagefright,target_disables_thumbnail_block_model,true)
+
 # Light
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.qti \

THE-EGO-999-GT/device_xiaomi_sm8350-common

Signed-off-by: THE-EGO-999-GT <theego999.me@gmail.com>

common.mk
@@ -289,6 +289,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/yupik/media_codecs_yupik_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_yupik_v1.xml \
     $(LOCAL_PATH)/media/yupik/media_profiles_yupik_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_yupik_v1.xml
 
+$(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
+
 # Native libraries whitelist
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt